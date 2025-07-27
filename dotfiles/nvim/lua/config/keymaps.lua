80a9bc76d90e2ae9c57d17d946ebbb175e037f0a
rezna <128091102+reznadev@users.noreply.github.com>
2025-07-28 01:56:40 +0200

    Update keymaps.lua

--- dotfiles/nvim/lua/config/keymaps.lua
+++ dotfiles/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- Create new tab with Ctrl+t
+vim.keymap.set("n", "<C-t>", ":tabnew<CR>", { desc = "New tab" })
+
+-- Your buffer keybindings
+vim.keymap.set("n", "<C-n>", ":bnext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "<C-p>", ":bprev<CR>", { desc = "Previous buffer" })
