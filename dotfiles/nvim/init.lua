10d2452d9e6724b865b25d1982932f1d646cd4fe
rezna <128091102+reznadev@users.noreply.github.com>
2025-07-28 01:56:24 +0200

    Update init.lua

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -1,9 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- Create new tab with Ctrl+t
-vim.keymap.set("n", "<C-t>", ":tabnew<CR>", { desc = "New tab" })
 
--- Your buffer keybindings
-vim.keymap.set("n", "<C-n>", ":bnext<CR>", { desc = "Next buffer" })
-vim.keymap.set("n", "<C-p>", ":bprev<CR>", { desc = "Previous buffer" })
